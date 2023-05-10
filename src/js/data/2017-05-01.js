8fba9fef0079aaff4fa5273c402e50d4c241ee7f
Jordy Goodridge <me@hazu.moe>
2023-05-10 09:50:00 -0700

    Remove trailing commas

--- src/js/data/2017-05-01.js
+++ src/js/data/2017-05-01.js
@@ -8,9 +8,9 @@ dataSet[dataSetVersion].options = [
     tooltip: "Check this to restrict to certain series.",
     checked: false,
     sub: [
-      { name: "Bombergirl", key: "Bombergirl" },
+      { name: "Bombergirl", key: "Bombergirl" }
     ]
-  },
+  }
 ];
 
 dataSet[dataSetVersion].characterData = [
@@ -18,140 +18,140 @@ dataSet[dataSetVersion].characterData = [
     name: "Shiro",
     img: "BIirmw4.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Oren",
     img: "AzKjrtZ.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Emera",
     img: "tlvTnOw.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Momoko",
     img: "RgN86Xu.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Kuro",
     img: "XBmyWil.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Urushi",
     img: "ED45ANr.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Paparu",
     img: "X6tRKqH.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Aqua",
     img: "M70olHJ.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Shiori",
     img: "HE43JOC.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Sepia",
     img: "khAJfks.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Tsugaru",
     img: "gBItjju.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Grim Aloe",
     img: "HDq7MFe.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Gray",
     img: "ReEpt9N.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Asagi",
     img: "tXQkdkX.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Pastel",
     img: "ignfhd6.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Pine",
     img: "Ykzk9As.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Shiron",
     img: "tncJdBb.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Tekka",
     img: "KIeGC3i.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Olive",
     img: "PlMkQpW.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
   },
   {
     name: "Prune",
     img: "23mbRYD.png",
     opts: {
-      series: ["Bombergirl"],
+      series: ["Bombergirl"]
     }
-  },
+  }
 ];
