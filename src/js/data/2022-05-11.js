e7f28e765a2d24de7313f18a628835dffd0964d9
Rendroken <rendroken@gmail.com>
2023-05-11 13:59:44 +0100

    New Girls
    Added arcade only characters, updated images and added new banner

---
+++ src/js/data/2022-05-11.js
@@ -0,0 +1,195 @@
+dataSetVersion = "2023-05-11"; // Change this when creating a new data set version. YYYY-MM-DD format.
+dataSet[dataSetVersion] = {};
+
+dataSet[dataSetVersion].options = [
+  {
+    name: "Filter by Class Type",
+    key: "class",
+    tooltip: "Check this to restrict to certain classes.",
+    checked: false,
+    sub: [
+      { name: "Bomber", key: "Bomber" },
+      { name: "Attacker", key: "Attacker" },
+      { name: "Shooter", key: "Shooter" },
+      { name: "Blocker", key: "Blocker" },
+    ]
+  },
+];
+
+dataSet[dataSetVersion].characterData = [
+  {
+    name: "Shiro",
+    img: "xM5fJ3V.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Oren",
+    img: "nX6hK8z.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+  {
+    name: "Emera",
+    img: "5CrAvA2.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Momoko",
+    img: "R6UihZL.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Kuro",
+    img: "GdH06i6.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Urushi",
+    img: "NL6MRi7.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+  {
+    name: "Paparu",
+    img: "Ln2zw9Q.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Aqua",
+    img: "Jn4r7AO.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Shiori",
+    img: "5rnvkk0.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Sepia",
+    img: "CfJYJSJ.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+  {
+    name: "Tsugaru",
+    img: "jgom2dz.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Grim Aloe",
+    img: "xpfInWL.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Gray",
+    img: "ncd1pHv.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Asagi",
+    img: "WGVxe4Y.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+  {
+    name: "Pastel",
+    img: "bb9fr7B.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Pine",
+    img: "Lt5mFGV.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Shiron",
+    img: "2CMQ5Nj.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Tekka",
+    img: "c4aHR4t.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+  {
+    name: "Olive",
+    img: "IgTMhMl.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Prune",
+    img: "FVneU0H.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Platinum",
+    img: "KG4WrKU.png",
+    opts: {
+      class: ["Bomber"],
+    }
+  },
+  {
+    name: "Silver",
+    img: "k2p2ihY.png",
+    opts: {
+      class: ["Shooter"],
+    }
+  },
+  {
+    name: "Blueberry",
+    img: "KItI0ru.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Melon",
+    img: "UHj3SxI.png",
+    opts: {
+      class: ["Blocker"],
+    }
+  },
+  {
+    name: "Chigusa",
+    img: "omMuttB.png",
+    opts: {
+      class: ["Attacker"],
+    }
+  },
+];
